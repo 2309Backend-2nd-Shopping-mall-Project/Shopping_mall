84cee5fcbd8e6b9ea561634bdfcac5aa148950b2
eosl7808 <eosl7808@naver.com>
2023-12-16 04:29:16 +0900

    ++swagger

--- Shopping_mall_Project/src/main/java/com/github/shopping_mall/shopping_mall_project/config/SwaggerConfig.java
+++ Shopping_mall_Project/src/main/java/com/github/shopping_mall/shopping_mall_project/config/SwaggerConfig.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @Configuration
 public class SwaggerConfig {
     @Bean
