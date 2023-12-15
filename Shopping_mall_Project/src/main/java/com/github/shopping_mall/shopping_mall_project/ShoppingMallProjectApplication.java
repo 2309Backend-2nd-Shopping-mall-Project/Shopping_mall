f52bcfb60e4f4d206c07fe7c9ae95fa7e29dd264
eosl7808 <eosl7808@naver.com>
2023-12-15 21:03:30 +0900

    기초뼈대

---
+++ Shopping_mall_Project/src/main/java/com/github/shopping_mall/shopping_mall_project/ShoppingMallProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.shopping_mall.shopping_mall_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallProjectApplication.class, args);
+    }
+
+}
---
+++ Shopping_mall_Project/src/test/java/com/github/shopping_mall/shopping_mall_project/ShoppingMallProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.shopping_mall.shopping_mall_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
